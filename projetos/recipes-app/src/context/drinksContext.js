db7c8d4db801bdc23a3705cb8b762f371cbc1a6e
andrebpessoa <andrebpessoa09@gmail.com>
2021-10-06 16:02:30 -0300

    update

--- projetos/recipes-app/src/context/drinksContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const drinksContext = createContext();
-
-export default drinksContext;
--- projetos/recipes-app/src/pages/ExploreDrinksArea/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ExploreDrinksArea() {
-  return <h1>Not Found</h1>;
-}
-
-export default ExploreDrinksArea;
